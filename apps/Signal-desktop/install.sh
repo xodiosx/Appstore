1fa9c63ef8ee0c9355b96c58ee29145f9125bbb5
GitHub Action <github-actions[bot]@users.noreply.github.com>
2026-03-15 01:08:13 +0000

    Update app versions with latest releases from GitHub

--- apps/Signal-desktop/install.sh
+++ apps/Signal-desktop/install.sh
@@ -2,7 +2,7 @@
 
 supported_arch="aarch64"
 run_cmd="/opt/Signal-Unofficial/signal-desktop-unofficial --no-sandbox"
-version="v8.1.0"
+version="v8.2.1"
 app_type="distro"
 supported_distro="all"
 page_url="https://github.com/dennisameling/Signal-Desktop"
